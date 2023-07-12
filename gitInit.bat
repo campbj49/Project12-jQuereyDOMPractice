ad436032a24be8d4f35a92f20aa4db5517ced75b
John Campbell <campbj49@gmail.com>
2023-07-12 17:16:30 -0400

    first commit to GitHub

---
+++ gitInit.bat
@@ -0,0 +1,7 @@
+git init
+git add --all
+git reset -- master/gitInit.bat
+git commit -m "first commit to GitHub"
+git branch -M main
+git remote add origin https://github.com/campbj49/Project12-jQuereyDOMPractice.git
+git push -u origin main
